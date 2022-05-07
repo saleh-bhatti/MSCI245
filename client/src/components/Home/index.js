f92464c5fef4f1c4a8f06070a11bbfce4f1df542
Saleh Bhatti <s72bhatt@uwaterloo.ca>
2022-05-07 19:34:22 -0400

    changed greeting

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSci245!
+                Hey world
               </React.Fragment>
             ) : (
               <React.Fragment>
